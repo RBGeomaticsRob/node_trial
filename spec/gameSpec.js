8882f0c2df4d8cfc8c988d76a7b295805251aab6
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-29 14:11:53 +0100

    Gutter Game test written

---
+++ spec/gameSpec.js
@@ -0,0 +1,12 @@
+var Game = require('../src/game')
+
+describe('gutter game', function(){
+  var game = new Game();
+
+  it('scores 0 on a gutter game', function(){
+    for(var i = 0; i< 20; i++){
+      game.roll(0)
+    };
+    expect(game.score).toEqual(0);
+  });
+});
